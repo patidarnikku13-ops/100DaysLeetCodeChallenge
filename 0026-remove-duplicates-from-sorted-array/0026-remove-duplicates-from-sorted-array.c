eb709a199bb19e77cb81ae9f7a90a475cd975237
Nikita <patidarnikku13@gmail.com>
2026-03-20 17:26:10 +0530

    Time: 0 ms (100%), Space: 12.9 MB (24.36%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.c
@@ -0,0 +1,12 @@
+int removeDuplicates(int* nums, int numsSize) {
+   int count = 1;
+   int k = nums[0];
+   for(int i = 1 ; i<=numsSize-1;i++) {
+    if(k!=nums[i]){
+      k=nums[i];
+      nums[count]=nums[i];//for unique value 
+       count++;
+    }
+   }
+   return count;
+}
