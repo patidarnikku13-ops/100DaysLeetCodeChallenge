1feceda575b256f6852a5c10ebe201f9bfc26302
Nikita <patidarnikku13@gmail.com>
2026-04-04 15:03:15 +0530

    Time: 0 ms (100%), Space: 44.9 MB (37.82%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0,right = nums.length-1;
+
+
+        while(left<=right){
+            int mid = left+(right - left)/2;
+
+            if(nums[mid] == target){
+                return mid;//avoid overflow
+            }
+            else if(nums[mid]<target){
+                left = mid+1;//search right half
+            }
+            else{
+                right = mid -1;//search left half
+            }
+        }
+        return left;//if not found,left is correct intersection index
+    }
+}
