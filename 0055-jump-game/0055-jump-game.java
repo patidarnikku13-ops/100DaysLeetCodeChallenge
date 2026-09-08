848b0ba1371c48ec7324d4c28bd07a506cc966ac
Nikita <patidarnikku13@gmail.com>
2026-09-08 22:22:53 +0530

    Time: 2 ms (89.09%), Space: 47.9 MB (42.59%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int farthest = 0;
+
+        for(int i = 0; i<nums.length;i++){
+            //can reach this index
+            if(i > farthest){
+                return false;
+            }
+            //update the position we can reach
+            farthest = Math.max(farthest, i+nums[i]);
+
+            // Already reached the last index
+            if(farthest >= nums.length - 1){
+                return true;
+            }
+        }
+        return true;
+    }
+}
