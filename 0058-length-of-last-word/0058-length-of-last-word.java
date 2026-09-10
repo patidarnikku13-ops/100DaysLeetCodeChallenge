68d68c9a22452acd8d646d3ee6f37fe169e8dcac
Nikita <patidarnikku13@gmail.com>
2026-09-10 20:18:13 +0530

    Time: 0 ms (100%), Space: 43.3 MB (16.08%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int count = 0;
+       int count = 0;
     for(int i = s.length() - 1; i>=0 ;i--){
         if(s.charAt(i) != ' '){
             count++;
@@ -9,6 +9,23 @@ class Solution {
               break;
         }
       }
-      return count;
+    return count;
+    
+
+    // or
+    // int count = 0;
+    // int n = s.length() - 1;
+
+    //  while (n >= 0 && s.charAt(n) == ' ') {
+    //     n--;
+    // }
+
+    // while(n >= 0 && s.charAt(n) != ' '){
+    //     count++;
+    //     n--;
+    // }
+    // return count;
+
+
     }
 }
